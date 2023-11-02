b7bc47bd3b01f387ba4e11d04d874b07bf90639a
ivanOrdAlv <ivordalv@gmail.com>
2023-11-02 17:36:55 +0100

    marker, style and sound
    In this part of the project I´ve been working on the marker that shows the points that the players have scored, the style of the page and of the canvas and the different sounds of the game

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ function moverBola() {
     context.clearRect(0, 0, myCanvas.width, myCanvas.height);
     context.beginPath();
     context.arc(bola.posicionBolaEjeX, bola.posicionBolaEjeY, 10, 0, 2 * Math.PI);
-    context.fillStyle = "white";
+    context.fillStyle = "black";
     context.fill();
     
     bola.posicionBolaEjeX += bola.velocidadX;
@@ -45,3 +45,35 @@ function pintarRectanguloCanvas(){
     context.fillStyle = "black";
     context.fillRect(0, 0, WIDTH, HEIGHT);
 }
+
+
+
+
+let marcador1;
+let marcador2;
+
+marcador1 =0;
+marcador2 =0;
+
+const winSound=new Audio('audio/audio/crowd-cheer-ii-6263.mp3');
+const hitSound=new Audio('audio/audio/laser-gun-72558.mp3');
+
+window.onload = function(){
+    requestAnimationFrame(update);
+   //Hola
+}
+
+
+function update(){
+    requestAnimationFrame(update);
+    context.clearRect(0, 0, WIDTH, HEIGHT);
+
+    context.font="45px Black Ops One ";
+    context.fillText(marcador1, WIDTH/5, 45);
+    context.fillText(marcador2, WIDTH*4/5 -45, 45);
+
+    for(let i=0; i < HEIGHT; i+=9){
+        context.fillRect(WIDTH/2 - 10, i, 5, 10);
+    }
+}
+
